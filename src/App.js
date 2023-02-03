4a00e05594a04775f7721eff5c432d624b1ed15d
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-02-03 10:06:57 +0700

    bt1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  let isUserLoggedIn = false ;
+  let status;
+    if (isUserLoggedIn==true) {
+        status = 'Hello User';
+    
+    } else {
+        status = 'Login Now';
+    }
+    return (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            { status }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    )
 }
 
 export default App;
